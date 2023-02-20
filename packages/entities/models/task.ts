7331a5e440c3395b367874b41bd1279202bf8d13
Omar <38073753+lordsayur@users.noreply.github.com>
2023-02-20 09:26:54 +0000

    ✨entity(model): add task model

---
+++ packages/entities/models/task.ts
@@ -0,0 +1,8 @@
+import { TaskStatus } from "../enums";
+import { ModelBase } from "./model-base";
+
+export class Task extends ModelBase {
+  public description: string;
+  public status: TaskStatus;
+  public parentTaskId?: number;
+}
