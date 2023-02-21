6f356d5bc25f06f83546568771cbd6a7cc186acb
Omar <38073753+lordsayur@users.noreply.github.com>
2023-02-21 05:10:23 +0000

    ✨ entities(model): add constructor to task

--- packages/entities/models/task.ts
+++ packages/entities/models/task.ts
@@ -5,4 +5,17 @@ export class Task extends ModelBase {
   public description: string;
   public status: TaskStatus;
   public parentTaskId: number | null;
+
+  constructor(
+    id: number,
+    createdByUserId: number,
+    description: string,
+    status?: TaskStatus,
+    parentTaskId?: number
+  ) {
+    super(id, createdByUserId);
+    this.description = description;
+    this.status = status ?? TaskStatus.ToDo;
+    this.parentTaskId = parentTaskId ?? null;
+  }
 }
